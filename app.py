0dd59a81b0c7994909199eafed64923fadb1df93
Bhavya <bhavyahshree@gmail.com>
2025-03-21 14:14:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 
 file_path = "BioinformaticsDashboardv0.0..Rmd"
 
-video_path = 'BioinformaticsDashboard v0.mp4'
-
 st.title("Please make sure you have RStudio and R installed before doing this!")
 st.write("Click the button below to download the BioinformaticsDashboard! Currently in version 0 🧬")
 
@@ -14,6 +12,3 @@ with open(file_path, "rb") as file:
         file_name=file_path,
         mime="application/Rmd" 
     )
-
-st.write("There is a very quick demo below to show what an example output might look like! 🦠")
-st.video(video_path)
